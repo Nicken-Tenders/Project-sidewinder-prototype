638c1a1ccd990ca5a034d1580861e098d98a0b5a
Nicken-Tenders <dominickbeim@gmail.com>
2021-10-27 17:08:06 -0700

    Completed the controls
    From school
    Started using the unity input manager, and assigned menu buttons and play buttons

--- Assets/Scripts/Player1Controller.cs
+++ Assets/Scripts/Player1Controller.cs
@@ -4,19 +4,68 @@ using UnityEngine;
 
 public class Player1Controller : MonoBehaviour
 {
+    public GameObject Player;
+    public float walkSpeed = 4;
 
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //if(KeyCode.A)
-        //{
+        if (Input.GetButton("Down"))
+        {
+            Debug.Log("Down!");
+        }
+        if (Input.GetButtonDown("Up"))
+        {
+            Debug.Log("Up!");
+        }
+        if (Input.GetButtonDown("Accept"))
+        {
+            Debug.Log("Accept!");
+        }
+        if (Input.GetButtonDown("Cancel"))
+        {
+            Debug.Log("Cancel!");
+        }
 
-        //}
+        if (Input.GetButtonDown("LP"))
+        {
+            Debug.Log("LP!");
+        }
+        if (Input.GetButtonDown("HP"))
+        {
+            Debug.Log("HP!");
+        }
+        if (Input.GetButtonDown("LK"))
+        {
+            Debug.Log("LK!");
+        }
+        if (Input.GetButtonDown("HK"))
+        {
+            Debug.Log("HK!");
+        }
+        if (Input.GetButton("MLeft") && !Input.GetButton("MRight"))
+        {
+            Player.transform.Translate(Vector2.left * walkSpeed * Time.deltaTime);
+        }
+        if (Input.GetButton("MRight") && !Input.GetButton("MLeft"))
+        {
+            Player.transform.Translate(Vector2.right * walkSpeed * Time.deltaTime);
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("Jump!");
+        }
+        if (Input.GetButtonDown("Start"))
+        {
+            Debug.Log("Start!");
+        }
+        if (Input.GetButtonDown("Select"))
+        {
+            Debug.Log("Select!");
+        }
     }
 }
